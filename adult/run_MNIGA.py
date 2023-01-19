e18bb14ed214399d02f1087136f8e26656c570fb
ltl7155 <tianlin001@e.ntu.edu.sg>
2023-01-19 02:07:18 +0000

    revise

--- adult/run_MNIGA.py
+++ adult/run_MNIGA.py
@@ -9,7 +9,7 @@ import os
 def task(gpuID, lam, T, exp, fair_method, mode):
     thread_name = threading.current_thread().getName()
     print(f'task_lam-{lam}_T-{T}_exp-{exp}：doing something')
-#     sh = f"CUDA_VISIBLE_DEVICES={gpuID} python main_prune_alexnet.py --fair_method MNIGA --mode eo --lam {lam} --exp {exp} --epochs 20 --T {T}"
+
     sh = f"CUDA_VISIBLE_DEVICES={gpuID} python main.py --fair_method {fair_method} --mode {mode} --lam {lam} --lam2 0.0 --exp {exp} --epochs 20 --T {T} --sl 1"
     os.system(sh)
     print(f'task_lam-{lam}_T-{T}_exp-{exp}：done')
@@ -35,12 +35,6 @@ def main():
                     gpuID = 2
                     pool.submit(task, gpuID, lam, T, exp, fair_method, mode)
 
-
-        
-#         sh = f"CUDA_VISIBLE_DEVICES={gpus[j%6]} nohup python repair/repair_compas.py --attr g\&r --p0 $[i] --p1 $[j] --acc_lb 0.5 --percent 0.3 --weight_threshold 0.2 2>&1 > compas_a\&r.log &"
-#         os.system(sh)
         
 if __name__ == '__main__':
     main()
-    
-# CUDA_VISIBLE_DEVICES=0 python main_prune.py --fair_method GapReg --mode eop --lam 0.01 --lam2 0.0 --exp 0 --epochs 10 --T 2 --sl 1
